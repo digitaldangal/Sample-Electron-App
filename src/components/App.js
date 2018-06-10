a013636a88909418414b0378d86a1cc3cda3718c
jasarqui <jzarquilita@up.edu.ph>
2018-06-11 07:43:25 +0800

    Added Material-UI dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,47 @@
 import '../assets/css/App.css';
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flex: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, Electron!</h1>
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+        <div style={styles.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                className={styles.menuButton}
+                color="inherit"
+                aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" style={styles.flex}>
+                Title
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <h1>POGI NI ARQUI!</h1>
+        <Button variant="contained"> Hello! </Button>
       </div>
     );
   }
